65ad3ebee11e441c28f8e9b44649eefdf7d397fc
Cisco Delgado <fdelgados@gmail.com>
2022-11-13 21:33:42 +0100

    Add env files support

---
+++ src/api/src/test/java/com/authoriza/ApiApplicationTest.java
@@ -0,0 +1,12 @@
+package com.authoriza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootTest
+public class ApiApplicationTest {
+    @Test
+    public void contextLoads() throws Exception {
+    }
+}
